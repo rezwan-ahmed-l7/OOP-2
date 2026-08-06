1e9226c784991e998d7df480ab7b92be509c31cd
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-07 00:07:51 +0600

    Added

---
+++ JAVA Codes/6th_Lab_Inheritance_&_this_Keyword/Exp_Two/Manager_&_Employee/Manager_&_Employee/src/ExpTwo/Main.java
@@ -0,0 +1,9 @@
+package ExpTwo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Manager m1 = new Manager(1, "Rahim Mama", 5000.00, 10000.00);
+        m1.showTotalSalary();
+    }
+}
